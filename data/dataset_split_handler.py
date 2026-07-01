e5ec9096043b8415409b22a8d38b13c843252a6f
Jiazheng Wang <81398113+wjiazheng@users.noreply.github.com>
2026-07-01 10:22:11 +0800

    Update dataset_split_handler.py

--- data/dataset_split_handler.py
+++ data/dataset_split_handler.py
@@ -1,8 +1,6 @@
 
 """ Convenient dataset splitting. Add new split functions here.  """
 
-__author__ = "Fabi Bongratz"
-__email__ = "fabi.bongratz@tum.de"
 
 from data.supported_datasets import SupportedDatasets
 from data.cortex import Cortex
