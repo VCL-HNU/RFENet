0a9f134af16bfb24b2a8a61154603ae88b58e9ff
Jiazheng Wang <81398113+wjiazheng@users.noreply.github.com>
2026-07-01 10:16:26 +0800

    Update model_names.py

--- utils/model_names.py
+++ utils/model_names.py
@@ -1,9 +1,6 @@
 
 """ Names of stored models. """
 
-__author__ = "Fabi Bongratz"
-__email__ = "fabi.bongratz@tum.de"
-
 # Model names
 INTERMEDIATE_MODEL_NAME = "intermediate.model"
 BEST_MODEL_NAME = "best.model"
